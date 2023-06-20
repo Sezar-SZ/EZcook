7b540f74495bba41b9b3dc929cd8a1c4526fdb19
Soheil <soheilzarepoor79@gmail.com>
2023-06-20 10:42:26 +0330

    fix: eslint error on backend

---
+++ apps/backend/.eslintrc.js
@@ -0,0 +1,31 @@
+// TODO: Eslint config doesn't get detected...
+
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        mocha: true,
+        node: true,
+    },
+    extends: ["plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "tsconfig.json",
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint", "prettier"],
+    ignorePatterns: ["**/lib/", "**/dist/"],
+    rules: {
+        "prettier/prettier": "error",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": [
+            "warn", // or "error"
+            {
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "^_",
+                caughtErrorsIgnorePattern: "^_",
+                ignoreRestSiblings: true,
+            },
+        ],
+    },
+};
