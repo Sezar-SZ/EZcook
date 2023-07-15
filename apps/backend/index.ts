c2ddc2aa5e5a32fb6295e32bf54649eb789def8e
Soheil <soheilzarepoor79@gmail.com>
2023-07-15 22:02:41 +0330

    fix some errors for prod

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,3 +1,3 @@
 export { loginSchema, type LoginDto } from "./src/auth/dto";
 export { createFoodSchema, type CreateFoodDto } from "./src/foods/dto";
-export { Food } from "@prisma/client";
+export { type Food } from "@prisma/client";
